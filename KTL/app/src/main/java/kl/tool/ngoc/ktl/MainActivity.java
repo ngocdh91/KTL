fda60639e7e5b2434c41d1b8db20353cc3fded36
NgocDh <th113023@gmail.com>
2016-11-29 10:53:09 +0700

    create

---
+++ KTL/app/src/main/java/kl/tool/ngoc/ktl/MainActivity.java
@@ -0,0 +1,13 @@
+package kl.tool.ngoc.ktl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
